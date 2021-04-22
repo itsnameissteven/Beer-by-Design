0003ed6b95135f6fa29c65566ec84a4b3abe69e3
Steven Mancine <stevenrmancine@gmail.com>
2021-04-22 13:03:42 -0600

    Install react router

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,7 @@ export const getAPIs = () => {
   return fetch('https://api.punkapi.com/v2/beers')
             .then(response => response.json());
 }
+
+export const searchAPI = () => {
+  return fetch('https://api.punkapi.com/v2/beers?')
+}
