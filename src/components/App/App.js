7e83fd2521bbf6f5e58093a6adcd0ea3f387ac4d
Steven Mancine <stevenrmancine@gmail.com>
2021-04-22 19:24:29 -0600

    Add search feature

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 
 import React, {useState, useEffect} from 'react';
-import { FeaturedBeers } from '../FeaturedBeers/FeaturedBeers'
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { FeaturedBeers } from '../FeaturedBeers/FeaturedBeers';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { getAPIs } from '../../apiCalls.js'
 import './App.css';
@@ -8,16 +9,30 @@ import './App.css';
 
 function App() {
   const [beerList, setBeerList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('')
 
   useEffect(() => {
     getAPIs().then(data => setBeerList(data))
   }, [])
 
+  const updateSearch = (query) => {
+    setSearchQuery(query);
+  }
   return (
     <div className="app">
       <h1 className="app__header">Brew by Design</h1>
-      <SearchBar />
-      <FeaturedBeers beerList={beerList.slice(0, 3)} />
+      <SearchBar updateSearch={updateSearch}/>
+      <Switch>
+        {searchQuery && <Redirect to={'/search/' + searchQuery} />}
+        <Route path="/" exact render={() => { 
+            return <FeaturedBeers beerList={beerList.slice(0, 3)} />
+        }}  
+        />
+        <Route exact path="/search/:search" render={({ match }) => {
+          return <h1>hey boo boo</h1>
+        }} />
+      </Switch>
+      
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
-import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 import './SearchBar.css';
 
-export const SearchBar = () => {
+export const SearchBar = ({updateSearch}) => {
   const [search, setSearch] = useState('')
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    updateSearch(search)
+  }
+
   return (
-    <form className='search'>
+    <form className='search' action={`/search/${search.trim()}`} onSubmit={handleSubmit}>
       <div className="search__container">
         <AiOutlineSearch className="search__icon"/>
         <input
           className="search__input"
-          type="search"
+          type="text"
           name="search"
           value={search}
           placeholder="Search for a beer recipe"
           onChange={(e) => setSearch(e.target.value)}
         />
         <AiOutlineClose className="search__clear" />
-        {/* Add a clear button here */}
       </div>
     </form>
   )
