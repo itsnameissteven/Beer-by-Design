2daea54128dd3a77cfd4e6928c026206c70a84c9
Steven Mancine <stevenrmancine@gmail.com>
2021-04-25 19:12:55 -0600

    Add mobile view to landing page

--- src/components/FeaturedBeers/FeaturedBeers.js
+++ src/components/FeaturedBeers/FeaturedBeers.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './FeaturedBeers.css'
-import  homebrew from '../../assets/homebrew.jpg'
+import { Link } from 'react-router-dom';
+import './FeaturedBeers.css';
+import  homebrew from '../../assets/homebrew.jpg';
 
 const FeaturedBeers = ({ beerList }) => {
   const featuredBeers = beerList.map((beer, i) => {
@@ -10,15 +11,18 @@ const FeaturedBeers = ({ beerList }) => {
         <h3 className="beer-card__name"> {beer.name}</h3>
         <p className="beer-card__tagline">{beer.tagline}</p>
         <p className="beer-card__description">{beer.description}</p>
-        <button className="beer-card__btn">See details</button>
+        <Link to={`/recipe/${beer.id}`} className="recipe-link-btn">
+        <button className="beer-card__btn">View Recipe</button>
+        </Link>
       </div>
     )
   })
+
   return( 
     <div className="featured-beers">
       <div className="img-container">
         <img className="hero-img" src={homebrew} alt="homebrew beer on a table"/>
-        <p className="hero-img-mantra">"Relax. Don't worry. And <br />have a homebrew" - Charlie Papazian</p>
+        <p className="hero-img-mantra">"Relax. Don't worry. And <br />have a homebrew." - Charlie Papazian</p>
       </div>
       <h2 className="featured-beers__header">Recipes in the Spotlight</h2>
       {featuredBeers}
